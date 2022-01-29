5d0e3e9d40671ae3e56ee5416d23eb34d78745d3
Nick Schleiff <nschleiff@sd7.net>
2022-01-29 15:42:35 -0800

    progress on charge attack

--- Assets/Scripts/AI/ChargeAttack.cs
+++ Assets/Scripts/AI/ChargeAttack.cs
@@ -6,12 +6,16 @@ using UnityEngine.AI;
 public class ChargeAttack : MonoBehaviour, AttackType
 {
     [Tooltip("How fast should the AI approach between attacks?")]
+    [SerializeField]
     private float approachSpeed;
+
+
 
     public void Attack(GameObject attacker)
     {
         WoofersAI ai = attacker.GetComponent<WoofersAI>();
         NavMeshAgent agent = ai.Agent;
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
 
 
     }
