c1be74c55795f3544018544c828752cc42f7adfd
Nick Schleiff <nschleiff@sd7.net>
2022-01-29 17:20:12 -0800

    Finished charge attack type

--- Assets/Scripts/AI/ChargeAttack.cs
+++ Assets/Scripts/AI/ChargeAttack.cs
@@ -9,7 +9,22 @@ public class ChargeAttack : MonoBehaviour, AttackType
     [SerializeField]
     private float approachSpeed;
 
+    [Tooltip("How fast should the AI move while dashing?")]
+    [SerializeField]
+    private float dashSpeed;
 
+    [Tooltip("How long should the dash attack last?")]
+    [SerializeField]
+    private float dashTime;
+
+    [Tooltip("How often should the AI attack?")]
+    [SerializeField]
+    private float attackFrequency;
+    private float timeSinceLastAttack = 0;
+
+    [Tooltip("How much damage should the attack do?")]
+    [SerializeField]
+    private int damage;
 
     public void Attack(GameObject attacker)
     {
@@ -17,6 +32,33 @@ public class ChargeAttack : MonoBehaviour, AttackType
         NavMeshAgent agent = ai.Agent;
         GameObject player = GameObject.FindGameObjectWithTag("Player");
 
+        timeSinceLastAttack += Time.deltaTime;
 
+
+        //Dash if time period is met
+        if (timeSinceLastAttack >= attackFrequency)
+        {
+            agent.speed = dashSpeed;
+            agent.SetDestination(attacker.transform.position + transform.forward * 10);
+
+            //If dash is long enough
+            if (timeSinceLastAttack >= attackFrequency + dashTime)
+            {
+                //reset timeSinceLastAttack
+                timeSinceLastAttack = 0;
+            }
+        }
+        else
+        {
+            //movement between dashes
+            agent.speed = approachSpeed;
+            agent.SetDestination(player.transform.position);
+        }
+
+    }
+
+    void AttackType.DoDamage(GameObject target)
+    {
+        target.GetComponent<PlayerHealth>().TakeDamage(damage);
     }
 }
